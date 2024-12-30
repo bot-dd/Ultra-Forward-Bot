0921ca4dba8c24cb02590aa58582902ddfafba6f
mmtg445 <mmtg445@gmail.com>
2024-12-30 21:58:49 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,13 +8,13 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "28528300")
     API_HASH = environ.get("API_HASH", "de0db40bc15ebea5b7ce123bf57a4eb9")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8032047153:AAHRBS4AXkjCrX1tllknb2p12wgwI6dInUg") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Rahat:EGbbbvweNl6EFzf6@rahat.pizkp.mongodb.net/?retryWrites=true&w=majority&appName=Rahat")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Rahat")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7534546565 6506692502').split()]
-    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002419150355'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002308754804") 
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7016105736').split()]
+    LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002312610528'))
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Movies_Squid_Game") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
