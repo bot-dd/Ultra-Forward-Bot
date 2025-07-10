b6a47482b624cb49bf7767e8418dec38a4904ba7
bot-dd <botdd445@gmail.com>
2025-07-11 01:04:08 +0600

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Rahat")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '7945551029').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002312610528'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/RM_Movie_Flix") 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/RMxBots") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
