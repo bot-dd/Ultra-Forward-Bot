c48e99bcb07c1df1f71ed7ce64a5a3deebce8c31
bot-dd <botdd445@gmail.com>
2025-07-11 00:04:50 +0600

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from os import environ
 class Config:
     API_ID = environ.get("API_ID", "26649585")
     API_HASH = environ.get("API_HASH", "588a3ea6fd01ae88bd2e10fed7d55b2c")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "7662526187:AAFk3ML1-sIWKaw1QrM1OqqxmxtBrf2GHXI") 
+    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
     BOT_SESSION = environ.get("BOT_SESSION", "Auto_Forward") 
     DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Rahat:EGbbbvweNl6EFzf6@rahat.pizkp.mongodb.net/?retryWrites=true&w=majority&appName=Rahat")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Rahat")
