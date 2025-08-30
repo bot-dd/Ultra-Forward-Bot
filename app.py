d63743b24dff7ce079765918a6e5abf8e6f54ef6
bot-dd <botdd445@gmail.com>
2025-08-30 20:40:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
+import threading
 from flask import Flask
+from pyrogram import Client
+
+# Flask App
 app = Flask(__name__)
 
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz 
-
 @app.route('/')
-def hello_world():
-    return 'Silicon'
+def home():
+    return "Silicon Bot is Running!"
 
+
+# Function to run Flask
+def run_flask():
+    app.run(host="0.0.0.0", port=8000)
+
+# Function to run Bot
+def run_bot():
+    bot.run()
 
 if __name__ == "__main__":
-    app.run()
-    
-#Dont Remove My Credit @Silicon_Bot_Update 
-#This Repo Is By @Silicon_Official 
-# For Any Kind Of Error Ask Us In Support Group @Silicon_Botz     
+    # Run Flask in separate thread
+    threading.Thread(target=run_flask).start()
+    # Run Bot in main thread
+    run_bot()
